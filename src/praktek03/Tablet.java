e8ca671920e625e37cf015ddadaed98c4c3a052d
Dedyset <dedy.smkn2bjb@gmail.com>
2018-04-08 12:04:15 +0800

    attribut dan method

--- src/praktek03/Tablet.java
+++ src/praktek03/Tablet.java
@@ -1,4 +1,11 @@
 package praktek03;
 public class Tablet extends Smartphone{
+    private double ukuranLayar;
+
+    void throttle(){
+        System.out.println("layar ganal ....");
+    }  
+    
     
 }
+
