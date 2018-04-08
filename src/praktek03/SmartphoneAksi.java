a764e429f4207555a4509479d3e3bac3238a7c4b
Dedyset <dedy.smkn2bjb@gmail.com>
2018-04-08 11:46:39 +0800

    menggunakan method get

--- src/praktek03/SmartphoneAksi.java
+++ src/praktek03/SmartphoneAksi.java
@@ -8,5 +8,12 @@ public class SmartphoneAksi {
         s.setHarga(1900000);
         
         s.cetakInfo();
+        
+        System.out.print("Merknya \t: ");
+        System.out.println(s.getMerk());
+        System.out.print("Warnanya \t: ");
+        System.out.println(s.getWarna());
+        System.out.print("Harganya \t: ");
+        System.out.println(s.getHarga());
     }
 }
