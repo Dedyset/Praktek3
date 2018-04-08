753e5331f05a74df06a149986a6bd36084478ab7
Dedyset <dedy.smkn2bjb@gmail.com>
2018-04-08 12:06:33 +0800

    getter and setter

--- src/praktek03/Tablet.java
+++ src/praktek03/Tablet.java
@@ -5,6 +5,14 @@ public class Tablet extends Smartphone{
     void throttle(){
         System.out.println("layar ganal ....");
     }  
+
+    public double getUkuranLayar() {
+        return ukuranLayar;
+    }
+
+    public void setUkuranLayar(double ukuranLayar) {
+        this.ukuranLayar = ukuranLayar;
+    }
     
     
 }
