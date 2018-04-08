136ee97f26aed3e1378242349a498d46ff5f7090
Dedyset <dedy.smkn2bjb@gmail.com>
2018-04-08 11:33:52 +0800

    Instance, buat atribut dan panggil method

--- src/praktek03/SmartphoneAksi.java
+++ src/praktek03/SmartphoneAksi.java
@@ -2,6 +2,11 @@ package praktek03;
 
 public class SmartphoneAksi {
     public static void main(String[] args) {
+        Smartphone s = new Smartphone();
+        s.merk="Samsung";
+        s.warna="Gold";
+        s.harga=2200000;
         
+        s.cetakInfo();
     }
 }
